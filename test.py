51002c56a6f61db6522ce5ca50f5f467f14ab445
VSvir <90447390+VSvir@users.noreply.github.com>
2024-04-13 22:58:52 +0300

    add files

---
+++ test.py
@@ -0,0 +1,15 @@
+﻿import pandas as pd
+import re
+
+df = {'Latin': [], 'Russian': []}
+with open('латинские выражения.txt', 'r', encoding='utf-8' ) as f:
+    text = f.readlines()
+    for line in text:
+        line = re.split(r'\.|!|\?', line)
+        df['Latin'].append(line[1])
+        df['Russian'].append(line[2])
+    df = pd.DataFrame(df)
+
+print(df.head())
+df.to_csv('latin.csv', encoding='UTF-8', index=False)
+    
